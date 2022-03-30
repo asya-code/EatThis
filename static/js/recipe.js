335f0f787c2782195a5443f6aa164ea4d4aabc4d
asya-code <77514066+asya-code@users.noreply.github.com>
2022-03-29 17:48:59 -0700

    updated seed_database.py with proper images for recipes

---
+++ static/js/recipe.js
@@ -0,0 +1,16 @@
+// const favBttn=document.querySelector("#add_fav");
+// favBttn.addEventListener('click', (evt) => {
+//     evt.preventDefault();
+    // fetch('/add_fav.json?recipe.recipe_id=${recipe.recipe_id}')
+    //     .then(response => response.json())
+    //     .then(responseData => {
+    //     document.querySelector('#recipe_id').recipe_id = responseData;
+    //     });
+// });
+const favBttn=document.querySelector('#add_fav');
+favBttn.addEventListener('click', (evt) => {
+    fetch('/add_fav')
+    .then (response
+    document.querySelector('#hidden_recipe_od').value
+    
+    });
